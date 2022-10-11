94351ad3abcfbf9119bfb0301ae4deddba4784d8
mayzulyoo <mayzulyoo@gmail.com>
2022-10-11 14:15:01 +0700

    aaaaaaa

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -3,10 +3,6 @@
     <div class="sgds-container is-fullhd">
       <div class="row is-mobile">
         <div class="col">
-          <a href="https://www.gov.sg" target="_blank">
-            <span class="sgds-icon sgds-icon-sg-crest"></span>
-            <span class="is-text">A Singapore Government Agency Website</span>
-          </a>
         </div>
       </div>
     </div>
